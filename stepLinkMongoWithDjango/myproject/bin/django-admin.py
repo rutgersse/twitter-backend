b51d7e26f09f7995e97057e53c9c3564f828dda7
Harsh Kothari <harshkothari410@gmail.com>
2015-11-06 00:49:13 -0500

    deleted

--- stepLinkMongoWithDjango/myproject/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/z/Desktop/django/stepLinkMongoWithDjango/myproject/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- stepLinkMongoWithDjango/myproject/twitter/nonrelblog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from djangotoolbox.fields import ListField
-
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    tags = ListField()
-    comments = ListField()
